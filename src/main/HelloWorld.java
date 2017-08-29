5084415a91fe5a2a13ca9347b8db0ab53ec7c169
MelWavestone <30470555+MelWavestone@users.noreply.github.com>
2017-08-29 15:22:46 +0200

    Update HelloWorld.java

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -23,7 +23,7 @@ public class HelloWorld
       this.name = name;
    }
    
-   public main() {
+   public main(String[] args) {
       name = "Mélanie";
       System.out.println("---------------------------------------------");
       System.out.println("   Bonjour " + name + ", comment vas tu?  ");
