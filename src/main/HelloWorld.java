f2c745d218291c3d2b08ea4aadfd16750b044ea6
MelWavestone <30470555+MelWavestone@users.noreply.github.com>
2017-08-29 14:40:45 +0200

    Create HelloWorld.java

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("---------------------------------------------");
+      System.out.println("      Bonjour Mélanie , comment vas tu?      ");
+      System.out.println("---------------------------------------------");
+                      
+   }
+}
